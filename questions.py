208dfd42864f3c96e361164862c7b54c222fe52b
Isabella Castañeda <isabellacasta2011@outlook.com>
2025-03-17 00:19:41 -0300

    Modificacion bug

--- questions.py
+++ questions.py
@@ -16,7 +16,7 @@ answers = [
 "// Esto es un comentario",
 "/* Esto es un comentario */",
 "-- Esto es un comentario",
-"# Esto es un comentarios",
+"# Esto es un comentario",
 ),
 ("=", "==", "!=", "==="),
 ]
@@ -27,21 +27,26 @@ for _ in range(3):
 # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
 # Se muestra la pregunta y las respuestas posibles
-print(questions[question_index])
-for i, answer in enumerate(answers[question_index]):
-    print(f"{i + 1}. {answer}")
+    print(questions[question_index])
+    for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
-for intento in range(2):
-    user_answer = int(input("Respuesta: ")) - 1
+    for intento in range(2):
+        user_answer = int(input("Respuesta: ")) - 1
 # Se verifica si la respuesta es correcta
-    if user_answer == correct_answers_index[question_index]:
-        print("¡Correcto!")
-    break
-else:
+        if user_answer == correct_answers_index[question_index]:
+            print("¡Correcto!")
+            break
+        elif user_answer < 1 or user_answer > 4:
+            print ('Respuesta no valida')
+            break
+        elif user_answer != int:
+            print ('Respuesta no valida')
+            break
+    else:
 # Si el usuario no responde correctamente después de 2 intentos,
 # se muestra la respuesta correcta
-    print("Incorrecto. La respuesta correcta es:")
-print(answers[question_index]
-[correct_answers_index[question_index]])
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index] [correct_answers_index[question_index]])
 # Se imprime un blanco al final de la pregunta
 print()
