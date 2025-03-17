f8b03c8e8f0bf576566150104e20d0b11ae8b209
Isabella Castañeda <isabellacasta2011@outlook.com>
2025-03-17 00:46:24 -0300

    Modificacion zip

--- questions.py
+++ questions.py
@@ -24,13 +24,13 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+# Combinamos las tres listas en una lista de tuplas
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-# Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-# Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+for question, answer_choices, correct_answer_index in questions_to_ask:
+    # Se muestra la pregunta y las respuestas posibles
+    print(question)
+    for i, answer in enumerate(answer_choices):
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -43,7 +43,7 @@ for _ in range(3):
         if user_answer < 0 or user_answer > 3:
             print ('Respuesta no valida')
             continue
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer_index:
             print("¡Correcto!")
             puntaje = puntaje + 1
             break
@@ -52,7 +52,7 @@ for _ in range(3):
 # se muestra la respuesta correcta
             puntaje = puntaje - 0.5 
             print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index] [correct_answers_index[question_index]])
+            print(answer_choices[correct_answer_index])
 # Se imprime un blanco al final de la pregunta
 print()
 print ('El puntaje total obtenido es de: ', puntaje)
